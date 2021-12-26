8077197716820c909ceb4c46ff153930b7bf3d89
jterry94 <terryj@iit.edu>
2021-12-26 17:14:13 -0600

    Working Version

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,12 +7,97 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @State var calculationString = ""
+    @State private var enableButton = true
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            TextEditor(text: $calculationString)
+            Divider()
+            Button("Begin Calculation", action: self.startCalculation)
+                .disabled(enableButton == false)
+        }
+        
+        
     }
+    
+    func startCalculation(){
+        
+        enableButton = false  
+        slowMath()
+    }
+    
+    func slowMath() {
+        
+        
+        
+        Task{
+            
+            await updateGUI(text: "Start time \(Date())\n")
+            
+            let _ = await withTaskGroup(of:  Void.self) { taskGroup in
+                
+        for i in stride(from: 0, through: 25, by: 1){
+            
+            taskGroup.addTask {
+                
+                
+            await slowSum(i: i, addend1: Double.random(in: 0.0...100.0), addend2: Double.random(in: 0.0...100.0), randomSleep: UInt32(Int.random(in: 1...5)))
+                
+            }
+            
+            
+            }
+        }
+            
+            await updateGUI(text: "End time \(Date())\n")
+            
+            await setButtonEnable(state: true)
+                                                 
+        }
+        
+    }
+    
+    @MainActor func setButtonEnable(state: Bool){
+        
+        if state {
+            
+            enableButton = true
+        }
+        else{
+            
+            enableButton = false
+        }
+        
+    }
+    
+    @MainActor func updateGUI(text:String){
+        
+        calculationString += text
+        
+    }
+    
+    func slowSum(i: Int, addend1: Double, addend2: Double, randomSleep: UInt32) async{
+        
+        let sum = addend1 + addend2
+        
+        sleep(randomSleep)
+        
+        let returnText = "The sum of the \(i)th addition \(addend1) and \(addend2) is \(sum) with sleep time \(randomSleep) sec.\n"
+        
+        await updateGUI(text: returnText)
+        
+        
+    }
+
+
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Shared/MathClass.swift
@@ -0,0 +1,33 @@
+//
+//  MathClass.swift
+//  MathClass
+//
+//  Created by Jeff_Terry on 12/26/21.
+//
+
+//import Foundation
+//import Swift
+//
+//class MathClass{
+//    
+//    @Published var addend1 = 0.0
+//    @Published var addend2 = 0.0
+//    @Published var sum = 0.0
+//    @Published var returnText = ""
+//    
+//    func slowSum(){
+//        
+//        sum = addend1 + addend2
+//        
+//        sleep(5000)
+//        
+//        returnText = "The sum of" + addend1 + "and" + addend2 + "is" + sum +"."
+//        
+//        
+//    }
+//    
+//    
+//    
+//}
+
+

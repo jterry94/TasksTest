5d3c6c4773a2820edf299587eb67e1339a4a7215
jterry94 <terryj@iit.edu>
2021-12-26 12:39:29 -0600

    Initial Commit

---
+++ Shared/TasksTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksTestApp.swift
+//  Shared
+//
+//  Created by Jeff_Terry on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct TasksTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

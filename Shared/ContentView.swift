ad6ca3dfd0bf48101150a1ed6c16d02204422690
jterry94 <terryj@iit.edu>
2021-12-26 18:12:38 -0600

    Using Task and TaskGroup to thread a calculation contained in a Swift class.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,15 +11,16 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var calculationString = ""
-    @State private var enableButton = true
+   // @State var calculationString = ""
+    //@State private var enableButton = true
+    @ObservedObject var myMathClass = MathClass()
     
     var body: some View {
         VStack{
-            TextEditor(text: $calculationString)
+            TextEditor(text: $myMathClass.calculationString)
             Divider()
             Button("Begin Calculation", action: self.startCalculation)
-                .disabled(enableButton == false)
+                .disabled(myMathClass.enableButton == false)
         }
         
         
@@ -27,72 +28,11 @@ struct ContentView: View {
     
     func startCalculation(){
         
-        enableButton = false  
-        slowMath()
+        myMathClass.enableButton = false
+        myMathClass.slowMath()
     }
     
-    func slowMath() {
-        
-        
-        
-        Task{
-            
-            await updateGUI(text: "Start time \(Date())\n")
-            
-            let _ = await withTaskGroup(of:  Void.self) { taskGroup in
-                
-        for i in stride(from: 0, through: 25, by: 1){
-            
-            taskGroup.addTask {
-                
-                
-            await slowSum(i: i, addend1: Double.random(in: 0.0...100.0), addend2: Double.random(in: 0.0...100.0), randomSleep: UInt32(Int.random(in: 1...5)))
-                
-            }
-            
-            
-            }
-        }
-            
-            await updateGUI(text: "End time \(Date())\n")
-            
-            await setButtonEnable(state: true)
-                                                 
-        }
-        
-    }
     
-    @MainActor func setButtonEnable(state: Bool){
-        
-        if state {
-            
-            enableButton = true
-        }
-        else{
-            
-            enableButton = false
-        }
-        
-    }
-    
-    @MainActor func updateGUI(text:String){
-        
-        calculationString += text
-        
-    }
-    
-    func slowSum(i: Int, addend1: Double, addend2: Double, randomSleep: UInt32) async{
-        
-        let sum = addend1 + addend2
-        
-        sleep(randomSleep)
-        
-        let returnText = "The sum of the \(i)th addition \(addend1) and \(addend2) is \(sum) with sleep time \(randomSleep) sec.\n"
-        
-        await updateGUI(text: returnText)
-        
-        
-    }
 
 
 }
